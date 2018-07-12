016696ac8493538054873237823f090db87fcc9e
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-12 17:32:07 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :hair_color, :eye_color
+  attr_accessor :name, :birthday :hair_color, :eye_color, 
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
