cd95e61836a71defe4b612b990e37ce018dc896e
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-12 17:26:41 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,6 @@ class Person
   attr_accessor :name, :hair_color, :eye_color
 
   def initialize(attributes)
-    @name = name
-    @hair_color = hair_color
-    @eye_color = eye_color
+    
+  end
 end
